0f79e408dcef38041f0c446ab3a1ab8dc68637ba
Angeloamenta <angeloamenta7@gmail.com>
2022-02-17 16:54:59 +0100

    aggiunto css prova

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.base')
 
 @section('content')
+<div class="container">
 @foreach ($movies as $movie)
-<div class="card">
-    <h2>Titolo</h2>
-    <h4>{{ $movie->title }}</h4>
-    <h2>Lingua</h2>
-    <h4>{{ $movie->nationality }}</h4>
-    <h2>data di uscita</h2>
-    <h4>{{ $movie->date }}</h4>
-    <h2>Voto</h2>
-    <h4>{{ $movie->vote }}</h4>
+    <div class="card">
+        <h2>Titolo</h2>
+        <h4>{{ $movie->title }}</h4>
+        <h2>Lingua</h2>
+        <h4>{{ $movie->nationality }}</h4>
+        <h2>data di uscita</h2>
+        <h4>{{ $movie->date }}</h4>
+        <h2>Voto</h2>
+        <h4>{{ $movie->vote }}</h4>
+    </div>
+    @endforeach
 </div>
-@endforeach
 @endsection
